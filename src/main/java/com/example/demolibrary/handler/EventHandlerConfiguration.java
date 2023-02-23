d95a3784dd39eb9aa45944c01f57e2257ec06268
syz51 <syzroy@gmail.com>
2023-02-23 14:08:10 +0000

    add jpa repositories scan

--- src/main/java/com/example/demolibrary/handler/EventHandlerConfiguration.java
+++ src/main/java/com/example/demolibrary/handler/EventHandlerConfiguration.java
@@ -1,16 +1,22 @@
 package com.example.demolibrary.handler;
 
+import java.util.List;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
+import org.springframework.boot.autoconfigure.domain.EntityScanPackages;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.messaging.converter.AbstractMessageConverter;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.orm.jpa.persistenceunit.PersistenceManagedTypes;
+import org.springframework.orm.jpa.persistenceunit.PersistenceManagedTypesScanner;
+import org.springframework.util.StringUtils;
 
 @Configuration
-@EnableJpaRepositories(basePackages = "com.example.demolibrary.repository")
-@EntityScan(basePackages = "com.example.demolibrary.model")
+@EnableJpaRepositories("com.example.demolibrary.repository")
 public class EventHandlerConfiguration {
 
   @Bean
@@ -21,6 +27,21 @@ public class EventHandlerConfiguration {
   @Bean
   public AbstractMessageConverter messageConverter() {
     return new MappingJackson2MessageConverter();
+  }
+
+  @Bean
+  public PersistenceManagedTypes persistenceManagedTypes(BeanFactory beanFactory,
+      ResourceLoader resourceLoader) {
+    return new PersistenceManagedTypesScanner(resourceLoader).scan(getPackagesToScan(beanFactory));
+  }
+
+  private static String[] getPackagesToScan(BeanFactory beanFactory) {
+    List<String> packages = EntityScanPackages.get(beanFactory).getPackageNames();
+    if (packages.isEmpty() && AutoConfigurationPackages.has(beanFactory)) {
+      packages = AutoConfigurationPackages.get(beanFactory);
+    }
+    packages.add("com.example.demolibrary.model");
+    return StringUtils.toStringArray(packages);
   }
 
 }
